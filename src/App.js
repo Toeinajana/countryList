e25fe709518f7e32a5159ba4049718169d646bea
Nantawan G <toeinajana@hotmail.com>
2020-08-12 13:31:53 +0700

    1st commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import CountryList from './components/CountryList';
+import Search from './components/Search';
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="container">
+        <center><h1>Country Filter</h1></center>
+        <Search />
+        <CountryList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Input from '@material-ui/core/Input';
+
+
+export default class Search extends Component {
+    render() {
+        return (
+           <Input
+           className="search-box"
+           placeholder="Enter Country Name"
+           />
+        );
+    }
+}
