f3259f0809d46f31f1a8090c85460f025eca313d
Nantawan G <toeinajana@hotmail.com>
2020-08-20 13:59:31 +0700

    working on filter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import CountryList from './components/CountryList';
 import Search from './components/Search';
+import CountryListWithFlag from './data/countrywithFlag.json';
 
 class App extends Component {
+
+  
+
   render() {
     return (
 
       <div className="container">
         <center><h1>Country Filter</h1></center>
-        <Search />
-        <CountryList />
+        <Search/>
+        <CountryList result ={Search.result} />
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import Input from '@material-ui/core/Input';
+// import countrylist from './CountryList';
+import countrylist from '../data/countrywithFlag.json';
 
 
-export default class Search extends Component {
-    render() {
+
+function Search(keyword) {
+
+        console.log(keyword)
+
+
+    
         return (
+           
            <Input
            className="search-box"
            placeholder="Enter Country Name"
+           onChange={(event) =>{Search(event.target.value)}}
+        //    onChange={(event) =>{console.log(event.target.value)}}
+           
            />
+       
+           
         );
     }
-}
+
+export default Search;
